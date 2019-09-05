531586c92e21c49bb3406c861139aea574782ba7
Bruno <blins44@gmail.com>
2019-09-05 11:40:16 -0300

    Criando BottomSheet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './img/nubank.png';
+import Drawer from '@material-ui/core/Drawer';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = React.useState({
+    bottom: false
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,10 +27,13 @@ function App() {
         <h1>Ser NuBank é ter uma vida financeira descomplicada.</h1>
       </div>
       <div className="buttons">
-        <button className="btn -primary">quero ser nubank</button>
+        <button onClick={toggleDrawer('bottom', true)} className="btn -primary">quero ser nubank</button>
         <button className="btn -secondary">já tenho convite</button>
         <button className="btn -login">login</button>
       </div>
+      <Drawer anchor="bottom" open={state.bottom} onClose={toggleDrawer('bottom', false)}>
+        <p>works!</p>
+      </Drawer>
     </div>
   );
 }
